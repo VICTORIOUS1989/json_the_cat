983cc8161cb70d8f9072de56db629325377a939d
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-06-04 10:33:36 +0000

    reafactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
-const breedName = process.argv.slice(2);
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-request(url + breedName, (error, response, body)=> {
-  if (error) {
-    return console.log(error);
+const fetchBreedDescription = function(breedName , callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
+
+  request(url + breedName , (error, response, body)=> {
+    if (error) {
+      callback(error, null);
+    }
+   const data = JSON.parse(body);
+   const obj = data[0];
+   if (obj) {
+    //console.log(obj.description);
+    callback(null, obj.description);
+  } else {
+    callback(`Failed to find breed ${breedName}`, null);
   }
- const data = JSON.parse(body);
- const obj = data[0];
- if (obj) {
-  console.log(obj.description);
-} else {
-  console.log(`Failed to find breed ${breedName}`);
-}
-});
+  });
+};
+
+module.exports = {fetchBreedDescription};
